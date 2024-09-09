5e54cb4fe4c3995ae95a27cc4dde3cdee89b1975
Carlos P <2123110286@untels.edu.pe>
2024-09-09 16:52:40 -0500

    semana3

---
+++ DAMSem03_CicloDeVida_Activity/app/src/main/java/com/example/damsem03_ciclodevida_activity/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.damsem03_ciclodevida_activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("Entramos al Método onRestart()");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("Entramos al Método onStart()");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("Entramos al Método onResume()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("Entramos al Método onPause()");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("Entramos al Método onStop()");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("Entramos al Método onDestroy()");
+    }
+}
